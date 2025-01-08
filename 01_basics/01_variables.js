1fa8b4b9eb2c7d2b1afd8e6b3f558eb2b6baead5
balkrishna2507 <119125837+balkrishna2507@users.noreply.github.com>
2025-01-08 06:44:02 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 454543
+let accountEmail = "hello@gmail.com";
+var accountPassword = "hello@123"
+accountCity = "Indore"
+
+//accountId = 2
+accountEmail = "hello@google"
+accountPassword = "sawanaaaa"
+accountCity = "jaipur"
+let accountState;
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+let name = "hello"
+let age = 14
+let isLoggedIn = false
+
+/* number 
+bigInt 
+String  = "" 
+boolean = true , false
+null = standalone value 
+undefined = 
+symbol = uniqueness
+
+*/
+console.log(typeof(null))
+console.log(typeof(undefined))
+
