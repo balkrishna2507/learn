f78c53390a69306def9fbdf79d0ca41988a7ba6f
balkrishna2507 <119125837+balkrishna2507@users.noreply.github.com>
2025-01-08 15:56:33 +0000

    conversion and operation

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,71 @@
+let score = false;
+// console.log(typeof(score));
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+/* 33 = 33
+    '33' = 33
+    "abc23" = NaN
+    true = 1;
+    false = 0
+*/
+let isLoggedIn = " " ;
+let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+/* 1 = true 
+0 = false 
+"" = false ;
+"hello" = true
+*/
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+
+// Operations
+let value = 3;
+let negvalue = -value 
+// console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**4);
+
+let str1 = "sarvodaya" 
+let str2 = " Academy"
+let str3 = str1 + str2 
+//console.log(str3);
+
+// console.log("2"+2)
+// console.log(2+2)
+// console.log(2+"2")
+// console.log("2"+2+2)
+// console.log(2+2+"2")
+
+//console.log(true+);
+// console.log(+true);
+// console.log(+"");
+
+num1 = num2 = num3 = 2+2 
+// console.table([num1,num2,num3])
+
+
+let gameCounter = 100
+
+console.log(gameCounter);
+console.log(++gameCounter);
+console.log(++gameCounter);
+console.log(gameCounter++);
+console.log(gameCounter++);
+
+
+
+
+
