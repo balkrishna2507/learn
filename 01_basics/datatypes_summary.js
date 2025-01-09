71f6dcbe4376ba2103f60553c51f5551ac851730
balkrishna2507 <119125837+balkrishna2507@users.noreply.github.com>
2025-01-09 05:03:55 +0000

    data types

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,34 @@
+//primitive data types
+
+// 7 types String , Number , Boolean , null , undefined , Symbol , BigInt
+
+//Reference data types 
+
+//Arrays , Objects , Functions
+
+//Symbol
+
+const id = Symbol("123")
+const id1 = Symbol("123")
+// console.log(id===id1);
+
+//BigInt
+
+ const bigNumber = 45454646557557n
+// console.log(bigNumber);
+
+//Array
+const  heroes = ["hero1","hero2","hero3"]
+
+let myObj = {
+    name:"hello",
+    age:12,
+    city:"indore"
+}
+
+const myFunction = function() {
+    console.log("hhello ");
+    
+}
+
+console.log(typeof bigNumber);
