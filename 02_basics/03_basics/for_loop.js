c0213e890756d29e30edcd6ac8831a2a5677910e
balkrishna2507 <119125837+balkrishna2507@users.noreply.github.com>
2025-01-12 13:46:17 +0000

    for loop

---
+++ 02_basics/03_basics/for_loop.js
@@ -0,0 +1,16 @@
+for (let i = 0; i < cars.length; i++) {
+    text += cars[i] + "<br>";
+  }
+
+
+  for (let i = 0; i < 5; i++) {
+    text += "The number is " + i + "<br>";
+  }
+
+
+  let i = 2;
+let len = cars.length;
+let text = "";
+for (; i < len; i++) {
+  text += cars[i] + "<br>";
+}
